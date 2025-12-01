13af31ba2bd0cf2d4bb943f0dd129d73a2031ccc
Vladimir Bataev <vbataev@nvidia.com>
2025-12-01 23:11:13 +0400

    Fix flake8 suggestions
    Signed-off-by: Vladimir Bataev <vbataev@nvidia.com>

--- nemo/collections/common/data/utils.py
+++ nemo/collections/common/data/utils.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import logging
+
 from dataclasses import fields, is_dataclass
 from typing import Any, Union
 
