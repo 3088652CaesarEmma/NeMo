88293538d0a5733b8172b052246f83d35896f847
Charlie Truong <chtruong@nvidia.com>
2026-01-16 13:12:04 -0600

    Fix test
    Signed-off-by: Charlie Truong <chtruong@nvidia.com>

--- tests/collections/llm/gpt/data/megatron/hyena/test_evo2_dataset.py
+++ tests/collections/llm/gpt/data/megatron/hyena/test_evo2_dataset.py
@@ -1105,7 +1105,7 @@ def test_evo2_dataset_getitem(monkeypatch):
             self.mmap_bin_files = True
             self.mock = True
             self.tokenizer = tokenizer
-            self.defer_npy_index_mmap
+            self.defer_npy_index_mmap = False
 
     mock_indexed_dataset = MockIndexedDataset()
 
