ca6274c1a12e87fd81c62afdf1ac12ad3486588d
He Huang <heh@nvidia.com>
2026-03-04 07:25:19 -0800

    add resolve_trainer_cfg
    Signed-off-by: He Huang <heh@nvidia.com>

--- examples/asr/speech_pretraining/masked_token_pred_pretrain.py
+++ examples/asr/speech_pretraining/masked_token_pred_pretrain.py
@@ -20,6 +20,7 @@ from nemo.collections.asr.models.ssl_models import EncDecDenoiseMaskedTokenPredM
 from nemo.core.config import hydra_runner
 from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
+from nemo.utils.trainer_utils import resolve_trainer_cfg
 
 
 """
@@ -51,7 +52,7 @@ python pretrain_masked_token_pred.py \
 def main(cfg):
     logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
 
-    trainer = pl.Trainer(**cfg.trainer)
+    trainer = pl.Trainer(**resolve_trainer_cfg(cfg.trainer))
     exp_manager(trainer, cfg.get("exp_manager", None))
     asr_model = EncDecDenoiseMaskedTokenPredModel(cfg=cfg.model, trainer=trainer)
 
