ef099630d5c9ba3ebbdeba5e9b89604138c2ae13
Edresson Casanova <edresson1@gmail.com>
2026-04-28 06:23:51 -0700

    Make trust_remote_code=False as default
    Signed-off-by: Edresson Casanova <edresson1@gmail.com>

--- nemo/collections/speechlm2/models/duplex_ear_tts.py
+++ nemo/collections/speechlm2/models/duplex_ear_tts.py
@@ -95,7 +95,7 @@ class DuplexEARTTS(LightningModule, HFHubMixin):
         self.tokenizer = AutoTokenizer(
             self.cfg.pretrained_lm_name,
             use_fast=True,
-            trust_remote_code=True,
+            trust_remote_code=self.cfg.get("trust_remote_code", False),
             bos_token=self.cfg.get("bos_token", None),
             eos_token=self.cfg.get("eos_token", None),
             pad_token=self.cfg.get("pad_token", None),
